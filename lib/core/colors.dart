831cdcc27da1dfb681e9016f569786884a31841d
denys-g7e <denyspastor04@gmail.com>
2026-06-26 21:56:13 -0400

    Add lib/core/colors.dart

---
+++ lib/core/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class QhipuColors {
+  QhipuColors._();
+
+  static const Color primaryGreen = Color(0xFF1a472a);
+  static const Color secondaryGreen = Color(0xFF2d6a4f);
+  static const Color lightGreen = Color(0xFF86efac);
+  static const Color mintGreen = Color(0xFFdcfce7);
+  static const Color amberAlert = Color(0xFFf59e0b);
+  static const Color amberBg = Color(0xFFfef3c7);
+  static const Color dangerRed = Color(0xFFdc2626);
+  static const Color dangerBg = Color(0xFFfee2e2);
+  static const Color surfaceMain = Color(0xFFf5f4f0);
+  static const Color cardWhite = Color(0xFFffffff);
+  static const Color borderSoft = Color(0xFFe0dfd8);
+  static const Color textMain = Color(0xFF1a1a1a);
+  static const Color textMuted = Color(0xFF888888);
+  static const Color textHint = Color(0xFFaaaaaa);
+}
